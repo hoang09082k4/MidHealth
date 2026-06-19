5d07a8d6fcae3234e39e6ac800ecc6b9d282a634
hoang09082k4 <lehoang09082k4@gmail.com>
2026-06-20 02:41:26 +0700

    Restore frontend Vercel API handler

---
+++ frontend/api/[...path].js
@@ -0,0 +1,4 @@
+module.exports = async function midhealthApi(request, response) {
+  const { handleRequest } = await import('../../backend/src/server.js');
+  return handleRequest(request, response);
+};
